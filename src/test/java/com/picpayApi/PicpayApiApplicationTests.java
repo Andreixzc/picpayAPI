aea7aa6d0694e6831383213d22de2dbed6fc3090
Andreixzc <andrei.massaini@hotmail.com>
2023-09-11 23:18:21 -0300

    first commit

---
+++ src/test/java/com/picpayApi/PicpayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpayApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
